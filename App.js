4888031839a84c1e2abcccae937c6ca2d3e0a524
Max Steele <max.steele@centre.edu>
2019-10-10 15:42:15 -0400

    started

--- App.js
+++ App.js
@@ -1,1 +1,28 @@
 
+var createBanner = function(message)
+{
+   d3.select("#banner")
+    .text(message)
+}
+
+var moviePromise = d3.json("https://ghibliapi.herokuapp.com/films")
+    moviePromise.then(
+             function(films)
+        {
+            createBanner("Studio Ghibli Films")
+        },
+             function(Error)
+        { 
+            createBanner("No Movies Here")
+        }
+    )
+
+var movieTitles = function(films)
+{
+    d3.select("#title")
+      .selectAll("p")
+      .data(films)
+      .enter()
+      .append("p")
+    
+}
