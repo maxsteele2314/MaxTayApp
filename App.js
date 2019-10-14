aecfb2b078b5d54e7d86610da9dfd6efaa0dd192
Tay Moss <tay.moss@centre.edu>
2019-10-14 19:57:27 -0400

    Finished lab

--- App.js
+++ App.js
@@ -1,28 +1,42 @@
+var moviePromise = d3.json("https://ghibliapi.herokuapp.com/films");
 
 var createBanner = function(message)
 {
-   d3.select("#banner")
-    .text(message)
+    d3.select(".banner").text(message);
 }
 
-var moviePromise = d3.json("https://ghibliapi.herokuapp.com/films")
-    moviePromise.then(
-             function(films)
-        {
-            createBanner("Studio Ghibli Films")
-        },
-             function(Error)
-        { 
-            createBanner("No Movies Here")
-        }
-    )
-
-var movieTitles = function(films)
+var movieTitles = function(movieData)
 {
-    d3.select("#title")
-      .selectAll("p")
-      .data(films)
-      .enter()
-      .append("p")
-    
+    d3.select(".titles")
+    .append("ul")
+    .selectAll("li")
+    .data(movieData)
+    .enter()
+    .append("li")
+    .text(function(d) { return d.title })
+    .on("click", function(d) { getData(d) });
 }
+
+var getData = function(movie)
+{
+    d3.select(".data *").remove("ul");
+    d3.select(".data").append("div").attr("class", "info");
+    d3.select(".info").append("ul").attr("class", "infoList");
+    d3.select(".infoList").append("li").text(movie.producer);
+    d3.select(".infoList").append("li").text(movie.director);
+    d3.select(".infoList").append("li").text(movie.rt_score);
+    d3.select(".infoList").append("li").text(movie.description);
+}
+
+moviePromise.then(
+function(movieData)
+{
+    createBanner("Movies");
+    movieTitles(movieData);
+    console.log("movieData", movieData);
+},
+function(err)
+{
+    createBanner("Loading Failed");
+    console.log(err);
+});
